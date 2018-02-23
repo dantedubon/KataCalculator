a14bfb6082b159005f78540f1e14c8d2a37d1c44
dantedubon <dante.dubon@gmail.com>
2018-02-23 16:05:10 -0600

    negative numbers failing

--- kata.js
+++ kata.js
@@ -1,5 +1,23 @@
 export default class Kata {
   add(numbers) {
-    throw "Not implemented";
+    if (numbers.length === 0) {
+      return 0;
+    } else if (numbers.length === 1) {
+      return parseInt(numbers);
+    } else if (numbers.substring(0, 2) == "//") {
+      const delimitador = numbers.substring(2, 3);
+      const resto = numbers.substring(3);
+      const arreglo = resto.split(delimitador);
+      const suma = arreglo.reduce(function(a, b) {
+        return parseInt(a) + parseInt(b);
+      });
+      return suma;
+    } else {
+      const arreglo = numbers.split(/[//n,]/);
+      const suma = arreglo.reduce(function(a, b) {
+        return parseInt(a) + parseInt(b);
+      });
+      return suma;
+    }
   }
 }
--- kata.test.js
+++ kata.test.js
@@ -6,4 +6,27 @@ describe("Kata Calculator", () => {
     const result = sut.add("");
     expect(result).toBe(0);
   });
+  it("Should return the same number", () => {
+    const result = sut.add("2");
+    expect(result).toBe(2);
+  });
+  it("Should sum two numbers", () => {
+    const result = sut.add("1,2");
+    expect(result).toBe(3);
+  });
+  it("Should sum any list of numbers", () => {
+    const result = sut.add("1,2,3");
+    expect(result).toBe(6);
+  });
+  it("Should sum any list of numbers with new line", () => {
+    const result = sut.add("1\\n2,3");
+    expect(result).toBe(6);
+  });
+  it("Should support different delimeter", () => {
+    const result = sut.add("//;\n1;2");
+    expect(result).toBe(3);
+  });
+  it("Should thrown an exception on negative numbers", () => {
+    expect(sut.add("-1")).toThrow("Negatives not allowed -1");
+  });
 });
